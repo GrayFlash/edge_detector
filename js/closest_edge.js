79984b1452af791dbf97c1c050f002a1b95826c3
GrayFlash <gk74533@gmail.com>
2021-03-25 08:30:03 +0530

    Update Y_co-ordinates value

--- js/closest_edge.js
+++ js/closest_edge.js
@@ -1,9 +1,13 @@
 let detected = document.getElementById('output-canvas');
 // console.log("Nope");
+var x_cor = 0;
+var y_cor = 0;
 detected.addEventListener("mousedown", function(e){
     let rect = detected.getBoundingClientRect(); 
     let x = e.clientX - rect.left; 
-    let y = e.clientY - rect.top; 
+    let y = e.clientY - rect.top -0.1875; 
     console.log("Coordinate x: " + x,
                 "Coordinate y: " + y);
+    
+    
 })
